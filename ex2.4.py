4774407580275784f94573f7f4cfde18c9153e60
ensluke <113048046+ensluke@users.noreply.github.com>
2023-02-12 22:45:39 -0700

    Edited quick sort function

---
+++ ex2.4.py
@@ -0,0 +1,23 @@
+def func1(arr, low, high):
+    if low < high:
+        pi = (low + high) // 2
+        arr[pi], arr[high] = arr[high], arr[pi]
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
+
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
